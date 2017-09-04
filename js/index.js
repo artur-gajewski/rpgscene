c473ab646bd61e3143106b63168ffc0fbdec491b
Artur Gajewski (fraktio) <artur.gajewski@fraktio.fi>
2017-09-04 20:29:02 +0300

    Fixed indentations

--- js/index.js
+++ js/index.js
@@ -648,24 +648,25 @@ function openHelpDialog() {
  */
 this.screenshotPreview = function(){
     xOffset = 10;
-	yOffset = 30;
-	$("a.screenshot").hover(function(e){
-		this.t = this.title;
-		this.title = "";
-		var c = (this.t != "") ? "<br/>" + this.t : "";
-		$("body").append("<p id='screenshot'><img width='200' src='"+ this.rel +"' alt='url preview' />"+ c +"</p>");
-		$("#screenshot")
-			.css("top",(e.pageY - xOffset) + "px")
-			.css("left",(e.pageX + yOffset) + "px")
-			.fadeIn("fast");
+    yOffset = 30;
+    $("a.screenshot").hover(function(e){
+        this.t = this.title;
+        this.title = "";
+        var c = (this.t != "") ? "<br/>" + this.t : "";
+        $("body").append("<p id='screenshot'><img width='200' src='"+ this.rel +"' alt='url preview' />"+ c +"</p>");
+        $("#screenshot")
+            .css("top",(e.pageY - xOffset) + "px")
+            .css("left",(e.pageX + yOffset) + "px")
+            .fadeIn("fast");
     },
-	function(){
-		this.title = this.t;
-		$("#screenshot").remove();
+    function(){
+        this.title = this.t;
+        $("#screenshot").remove();
     });
-	$("a.screenshot").mousemove(function(e){
-		$("#screenshot")
-			.css("top",(e.pageY - xOffset) + "px")
-			.css("left",(e.pageX + yOffset) + "px");
-	});
+
+    $("a.screenshot").mousemove(function(e) {
+        $("#screenshot")
+            .css("top",(e.pageY - xOffset) + "px")
+            .css("left",(e.pageX + yOffset) + "px");
+    });
 };
